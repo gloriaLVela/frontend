63b20d4852e4a747e921b79cf9064b7d8280fe05
Gloria L Vela <gloria.laura.vela@gmail.com>
2019-02-01 15:32:59 -0600

    Displaying and Editing Data in Angular with ASP.NET Core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+import { ApiService } from './api.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { QuestionComponent } from './question.component';
+import { QuestionsComponent } from './questions.component';
+import { QuizComponent } from './quiz.component';
+import { QuizzesComponent } from './quizzes.component';
+import { NavComponent } from './nav.component';
+
+
+
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'question/:quizId', component: QuestionComponent },
+  { path: 'questions', component: QuestionsComponent },
+  { path: 'quiz', component: QuizComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuestionComponent,
+    QuestionsComponent,
+    QuizComponent,
+    QuizzesComponent,
+    NavComponent
+
   ],
+
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes),
+
+
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
